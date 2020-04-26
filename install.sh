7425753856bd2be906d576af3fff183e7976f4fc
h0me5k1n <h0me5k1n@gmail.com>
2020-04-26 08:52:51 +0100

    Created install script
    Added variables around Github source and created install script
    including README instructions

--- install.sh
+++ install.sh
@@ -1,44 +1,79 @@
 #!/bin/sh
 
-source /usr/sbin/helper.sh
-
 # Absolute path to this script, e.g. /home/user/bin/foo.sh
 SCRIPT=$(readlink -f "$0")
 # Absolute path this script is in, thus /home/user/bin
 SCRIPTPATH=$(dirname "$SCRIPT")
-# load standard variables
-source "$SCRIPTPATH/addon_vars"
+# addon name
+MY_ADDON_NAME=nordvpnmanager
+# Github repo name
+GIT_REPO="asusmerlin-nvpnmgr"
+# Github repo branch - modify to pull different branch 
+# (fetch will overwrite local changes)
+GIT_REPO_BRANCH=master
+# Github dir
+GITHUB_DIR="https://raw.githubusercontent.com/h0me5k1n/$GIT_REPO/$GIT_REPO_BRANCH"
+# Local repo dir
+LOCAL_REPO="/jffs/scripts/$MY_ADDON_NAME"
+
+# functions
+errorcheck(){
+ echo "$SCRIPTSECTION reported an error..."
+ exit 1
+}
+
+# use to download the files from github
+GetFiles(){
+ GETFILENAME=addon_vars
+ SCRIPTSECTION=get_$GETFILENAME
+ [ -f "$LOCAL_REPO/$GETFILENAME" ] && rm "$LOCAL_REPO/$GETFILENAME"
+ wget -O "$LOCAL_REPO/$GETFILENAME" "$GITHUB_DIR/$GETFILENAME" >/dev/null 2>&1 || errorcheck
+
+ GETFILENAME=nordvpnmanager.sh
+ SCRIPTSECTION=get_$GETFILENAME
+ [ -f "$LOCAL_REPO/$GETFILENAME" ] && rm "$LOCAL_REPO/$GETFILENAME"
+ wget -O "$LOCAL_REPO/$GETFILENAME" "$GITHUB_DIR/$GETFILENAME" >/dev/null 2>&1 || errorcheck
+ chmod 755 "$LOCAL_REPO/$GETFILENAME"
+
+ GETFILENAME=ui_install.sh
+ SCRIPTSECTION=get_$GETFILENAME
+ [ -f "$LOCAL_REPO/$GETFILENAME" ] && rm "$LOCAL_REPO/$GETFILENAME"
+ wget -O "$LOCAL_REPO/$GETFILENAME" "$GITHUB_DIR/$GETFILENAME" >/dev/null 2>&1 || errorcheck
+ chmod 755 "$LOCAL_REPO/$GETFILENAME"
+
+ SCRIPTSECTION=
+}
+
+# Check this is an Asus Merlin router
+nvram get buildinfo | grep merlin >/dev/null 2>&1
+if [ $? != 0 ]
+then
+    echo "This script is only supported on an Asus Router running Merlin firmware!"
+    exit 5
+fi
 
 # Does the firmware support addons?
 nvram get rc_support | grep -q am_addons
 if [ $? != 0 ]
 then
+    echo "This firmware does not support addons!"
     logger "$MY_ADDON_NAME addon" "This firmware does not support addons!"
     exit 5
 fi
 
-# Obtain the first available mount point in $am_webui_page
-am_get_webui_page /jffs/addons/$MY_ADDON_NAME/$MY_ADDON_PAGE
-
-if [ "$am_webui_page" = "none" ]
+# Check jffs is enabled
+JFFS_STATE=$(nvram get jffs2_on)
+if [ $JFFS_STATE != 1 ]
 then
-    logger "$MY_ADDON_NAME addon" "Unable to install $MY_ADDON_PAGE"
+    echo "This addon requires jffs to be enabled!"
+    logger "$MY_ADDON_NAME addon" "This addon requires jffs to be enabled!"
     exit 5
 fi
-logger "$MY_ADDON_NAME addon" "Mounting $MY_ADDON_PAGE as $am_webui_page"
 
-# Copy custom page
-cp /jffs/addons/$MY_ADDON_NAME/$MY_ADDON_PAGE /www/user/$am_webui_page
+# create local repo folder
+mkdir -p "$LOCAL_REPO"
 
-# Copy menuTree (if no other script has done it yet) so we can modify it
-if [ ! -f /tmp/menuTree.js ]
-then
-    cp /www/require/modules/menuTree.js /tmp/
-    mount -o bind /tmp/menuTree.js /www/require/modules/menuTree.js
-fi
+# Get files
+GetFiles
 
-# Insert link at the end of the Tools menu.  Match partial string, since tabname can change between builds (if using an AS tag)
-sed -i "/url: \"Tools_OtherSettings.asp\", tabName:/a {url: \"$am_webui_page\", tabName: \"$MY_ADDON_TAB\"}," /tmp/menuTree.js
-
-# sed and binding mounts don't work well together, so remount modified file
-umount /www/require/modules/menuTree.js && mount -o bind /tmp/menuTree.js /www/require/modules/menuTree.js
+echo "installation complete... visit https://github.com/h0me5k1n/asusmerlin-nvpnmgr for usage information"
